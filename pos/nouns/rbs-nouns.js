4d5a6a82c007523ef78d8aa8aaff48d2da63a93d
Rachel Sterneck <rachel.sterneck@gmail.com>
2018-01-23 14:08:24 -0500

    Nifty nouns for madlibs

---
+++ pos/nouns/rbs-nouns.js
@@ -0,0 +1,12 @@
+module.exports.nouns = [
+	'Oprah Winfrey',
+	'Spongebob Squarepants',
+	'Rock Bottom',
+	'Sea World',
+	'Rosetta Stone',
+	'Mona Lisa',
+	'Omaha',
+	'Beyoncé',
+	'The Wall',
+	'Arya Stark'
+];
